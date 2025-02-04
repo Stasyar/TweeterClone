f524cbf64fadc9cf2ce99bb650cbb1a7ac487f0d
Stasya <ap1050167@gmail.com>
2025-02-04 12:16:04 +0400

    initial commit

--- app/routes/tweet.py
+++ app/routes/tweet.py
@@ -13,6 +13,4 @@ def hello():
 @router.post("/tweet", response_model=TweetCreateResponse, status_code=201)
 async def post_tweet(data: TweetCreateRequest):
 
-    # TODO вставить добавление твита в бд (бд пока нет)
-
     return {"result": True, "tweet_id": 123}
